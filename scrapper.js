906238b026cda5ec15b554cab9cc6db8c03cd3fe
Matt <matt.yost2@gmail.com>
2016-10-27 01:53:30 -0500

    initial commit
    Might change the csv

---
+++ scrapper.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function (){
+
+  const fs = require('fs');
+  const cheerio = require('cheerio');
+  const request = require('request');
+  const csv = require('csv-stringify');
+
+// The end
+})();
